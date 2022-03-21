28ec0629197002ed1fd3e95e1a07a3ba1dcf0a63
Andrew ASHBY <andrew.ashby.91@gmail.com>
2022-03-21 11:48:49 +0100

    Rajout de la classe WriteSymptomDataResultOut

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,5 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-import java.util.Map;
-
-
 public class AnalyticsCounter {
 
 
@@ -13,15 +9,11 @@ public class AnalyticsCounter {
          * its contents in an alphabetically arranged collection and we write the result in
          * the file:"result.out"
          */
-        ReadSymptomDataFromFile result = new ReadSymptomDataFromFile("symptoms.txt");
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("result.out");
-            for (Map.Entry<String, Integer> entry : result.getSymptoms().entrySet()) {
-                writer.write(" " + entry.getKey() + ":" + entry.getValue() + "\n");
-            }
-            writer.close(); //fermeture du fichier "result.out"
-        } catch (Exception e) {
+        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+        WriteSymptomDataResultOut writer = new WriteSymptomDataResultOut();
+        try{
+            writer.writeSymptoms(reader.getSymptoms());
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataResultOut.java
@@ -0,0 +1,30 @@
+package com.hemebiotech.analytics;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class WriteSymptomDataResultOut{
+
+    public WriteSymptomDataResultOut(){
+
+    }
+
+    public void writeSymptoms(TreeMap<String, Integer> data) throws IOException {
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("result.out");
+            for (Map.Entry<String, Integer> entry : data.entrySet()) {
+                writer.write(" " + entry.getKey() + ":" + entry.getValue() + "\n");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            writer.close(); //fermeture du fichier "result.out"
+        }
+
+    }
+}
